4447a649834cf3b15d21cafdfd74941f58fe3596
Drew Risinger <drewrisinger@users.noreply.github.com>
2021-03-03 16:46:21 -0500

    python3Packages.setuptools-rust: remove

--- default.nix
+++ default.nix
@@ -57,7 +57,7 @@ rec {
     qutip = pkgs.python3.pkgs.callPackage ./pkgs/python-modules/qutip { };  # removed from nixpkgs b/c it was broken (presumably unused)
     openfermion = pkgs.python3.pkgs.callPackage ./pkgs/python-modules/openfermion { inherit pubchempy; };
     # openfermion-cirq has been deprecated. Its functionality is now rolled into openfermion as of v1.0
-    setuptools-rust = pkgs.python3.pkgs.callPackage ./pkgs/python-modules/setuptools-rust { };
+    # setuptools-rust has been removed b/c it has been integrated into nixpkgs. That probably has a better derivation to copy
     tweedledum = pkgs.python3.pkgs.callPackage ./pkgs/python-modules/tweedledum { };
 
     # VISA & Lab Instrument control
