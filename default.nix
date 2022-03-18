5a6cc475fe0960a11232f98023fbd74298ab2b5b
Drew Risinger <drewrisinger@users.noreply.github.com>
2022-03-18 14:19:27 -0400

    python3Packages.csaps: init at 1.1.0

--- default.nix
+++ default.nix
@@ -38,6 +38,7 @@ rec {
     # New packages NOT in NixOS/nixpkgs (and likely never will be)
     asteval = pkgs.python3.pkgs.callPackage ./pkgs/python-modules/asteval { };
     autoray = pkgs.python3.pkgs.callPackage ./pkgs/python-modules/autoray { };
+    csaps = pkgs.python3.pkgs.callPackage ./pkgs/python-modules/csaps { };
     dynaconf = pkgs.python3.pkgs.callPackage ./pkgs/python-modules/dynaconf { };
     # nose-timer = pkgs.python3.pkgs.callPackage ./pkgs/python-modules/nose-timer { };
     oitg = pkgs.python3.pkgs.callPackage ./pkgs/python-modules/oitg { };
